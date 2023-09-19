5fc001376064e2e68f499fe666cd1e004b9e829e
NikhilRachha <85298555+NikhilRachha@users.noreply.github.com>
2023-09-19 16:50:33 +0530

    Add files via upload

---
+++ WebApiCaseStudyUsingJWT/Controllers/AuthenticateController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebAppSecurityProject.Models;
+
+namespace WebAppSecurityProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    [AllowAnonymous]
+    public class AuthenticateController : ControllerBase
+    {
+        public List<UserModel> usersList = null;
+        public AuthenticateController()
+        {
+            usersList = new List<UserModel>()
+            {
+                new UserModel() { UserId = 1, UserName = "Nikhil", Password = "1858", Role = "Admin" },
+                new UserModel() { UserId = 2, UserName = "Ved", Password = "Ved123", Role = "User" }
+            };
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel requestUser)
+        {
+            UserModel userObj = usersList.Where(x => x.UserName == requestUser.UserName && x.Password == requestUser.Password).FirstOrDefault();
+
+            if (userObj != null)
+            {
+                string tokenStr = GenerateJSONWebToken(userObj);
+                return Ok(new { token = tokenStr });
+            }
+            else
+            {
+                return BadRequest("Invalid user id or password");
+            }
+        }
+
+        private string GenerateJSONWebToken(UserModel userObj)
+        {
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysuperdupersecret"));
+            SigningCredentials credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+
+            List<Claim> authClaims = new List<Claim>
+            {
+                    new Claim(ClaimTypes.NameIdentifier,Convert.ToString(userObj.UserId)),
+                    new Claim(ClaimTypes.Name, userObj.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),  // (JWT ID) Claim
+                    new Claim(ClaimTypes.Role, userObj.Role)
+             };
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                        issuer: "mySystem",
+                        audience: "myUsers",
+                        claims: authClaims,
+                        expires: DateTime.Now.AddMinutes(10),
+                        signingCredentials: credentials);
+
+            string tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenString;
+        }
+    }
+}
---
+++ WebApiCaseStudyUsingJWT/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppSecurityProject.Models;
+
+namespace WebAppSecurityProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    [Authorize(Roles = "Admin")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            List<Product> prdList = new List<Product>() {
+                new Product(){ ProductId = 1022, ProductName = "Printer", UnitPrice = 2400, Quantity = 5 },
+                new Product(){ ProductId = 1023, ProductName = "Keyboard", UnitPrice = 1200, Quantity = 3 },
+                new Product(){ ProductId = 1024, ProductName = "iBall Mouse", UnitPrice = 890, Quantity = 4 },
+                new Product(){ ProductId = 1025, ProductName = "Boat Speakers", UnitPrice = 1900, Quantity = 6 },
+                new Product(){ ProductId = 1026, ProductName = "DLink Router", UnitPrice = 1350, Quantity = 2 }
+            };
+
+            return Ok(prdList);
+        }
+    }
+}
