548b8886350cc0d097b5e6aa7a7c97f35b1102e7
NikhilRachha <85298555+NikhilRachha@users.noreply.github.com>
2023-09-20 14:46:33 +0530

    Add files via upload

---
+++ Web Api/Controllers/ProductApiController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static WebApplication6.Models.EntityModelClasses;
+
+namespace WebApplication6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+
+        private ProductDbContext dbcontext;
+
+        public ProductApiController(ProductDbContext dbContext)
+        {
+            dbcontext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(dbcontext.Products.ToList());
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            Product product = dbcontext.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound(new { result = "not Found" });
+
+
+
+            }
+            else
+            {
+                return Ok(product);
+            }
+        }
+   
+     
+        [HttpPost]
+        public IActionResult Create([FromBody] Product product)
+        {
+            dbcontext.Products.Add(product);
+            dbcontext.SaveChanges();
+            return Ok(new { result = "Created new row" });
+        }
+        [HttpPut]
+        public IActionResult Edit([FromBody] Product obj)
+        {
+            dbcontext.Entry(obj).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            dbcontext.SaveChanges();
+            return Ok(new { result = "Updated Succesfully" });
+        }
+        [HttpDelete]
+        public IActionResult Delete([FromBody] int i)
+        {
+            Product obj = dbcontext.Products.Find(i);
+            dbcontext.Products.Remove(obj);
+            dbcontext.SaveChanges();
+            return Ok(new { result = "Deleted" });
+        }
+    }
+}
---
+++ Web Api/Models/EntityModelClasses.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication6.Models
+{
+    public class EntityModelClasses
+    {
+        public class Product
+        {
+            public int Id { get; set; }
+            public string ProductName { get; set; }
+            public int Quantity { get; set; }
+            public int UnitPrice { get; set; }
+            public string Category { get; set; }
+
+        }
+
+        public class ProductDbContext : DbContext
+        {
+            public DbSet<Product> Products { get; set; }
+
+            public ProductDbContext(DbContextOptions<ProductDbContext> options)
+             : base(options)
+            {
+
+            }
+
+        }
+    }
+}
